567ad5610c4a033b8fdf35a752bbd73ab02e5518
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2025-10-02 21:13:07 +0100

    Replace CopyOnWriteArrayList and ExecutorService

--- app/src/main/java/com/javacakegames/pride/SoundManager.java
+++ app/src/main/java/com/javacakegames/pride/SoundManager.java
@@ -7,17 +7,16 @@ import android.content.Context;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
 import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.TimerTask;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SoundManager {
 
-  private final CopyOnWriteArrayList<Sound> sounds;
+  private final ArrayList<Sound> sounds;
   private final List<Sound> soundsPool;
   private final SoundPool soundPool;
 
@@ -27,13 +26,13 @@ public class SoundManager {
   private float startVolume;
   private int streamCounter;
 
-  private final ExecutorService executorService;
+  private final Handler soundHandler;
 
   public SoundManager(Context context) {
 
     int MAX_STREAMS = 24; // 32 is OS limit
 
-    sounds = new CopyOnWriteArrayList<>();
+    sounds = new ArrayList<>();
     soundsPool = new ArrayList<>(MAX_STREAMS);
 
     this.maxSystemVolume = audioMan.getStreamMaxVolume(STREAM_MUSIC);
@@ -52,88 +51,82 @@ public class SoundManager {
     }
     soundId = soundPool.load(context, R.raw.gb4, 1);
 
-    Globals.TIMER.schedule(new SystemVolumeTask(), 0, 1000);
-    Globals.TIMER.schedule(new StopTask(), 0, 50);
+    HandlerThread soundThread = new HandlerThread("");
+    soundThread.start();
+    soundHandler = new Handler(soundThread.getLooper());
 
-    executorService = Executors.newCachedThreadPool();
+    Runnable systemVolumeRunnable = new Runnable() {
+      @Override
+      public void run() {
+        if (Globals.IS_ARC) startVolume = .33333333333333333333f;
+        else {
+          if (Build.VERSION.SDK_INT >= 5 && !audioMan.isWiredHeadsetOn()) {
+            startVolume = Math.min(Globals.TWO_THIRDS, // Louder for internal speaker
+              (float) audioMan.getStreamVolume(STREAM_MUSIC) / maxSystemVolume);
+          } else {
+            startVolume = 1;
+          }
+        }
+        soundHandler.postDelayed(this, 1000);
+      }
+    };
 
+    Runnable stopRunnable = new Runnable() {
+      @Override
+      public void run() {
+        Iterator<Sound> iterator = sounds.iterator();
+        while (iterator.hasNext()) {
+          Sound sound = iterator.next();
+          if (sound.fading) {
+            sound.currVolume -= sound.volumeStep;
+            float vol = Math.max(0, sound.currVolume);
+            if (vol == 0) {
+              soundPool.stop(sound.osId);
+              iterator.remove();
+              sound.free = true;
+            } else {
+              soundPool.setVolume(sound.osId, vol, vol);
+            }
+          }
+        }
+        soundHandler.postDelayed(this, 50);
+      }
+    };
+    soundHandler.post(systemVolumeRunnable);
+    soundHandler.post(stopRunnable);
   }
 
   public int play(float pitch) {
-
     Sound soundToAdd = null;
     for (Sound sound : soundsPool) {
       if (sound.free) {
-        sound.init(startVolume, streamCounter);
-        streamCounter++;
+        sound.init(startVolume, streamCounter++);
         soundToAdd = sound;
         break;
       }
     }
     if (soundToAdd == null) {
-      soundToAdd = new Sound(startVolume, streamCounter);
-      streamCounter++;
+      soundToAdd = new Sound(startVolume, streamCounter++);
       soundsPool.add(soundToAdd);
     }
     sounds.add(soundToAdd);
 
     Sound finalSoundToAdd = soundToAdd;
-    executorService.submit(() -> {
-      finalSoundToAdd.osId = soundPool.play(soundId, startVolume, startVolume, 0, 0, pitch);
-    });
+    soundHandler.post(() -> finalSoundToAdd.osId =
+      soundPool.play(soundId, startVolume, startVolume, 0, 0, pitch));
 
     return soundToAdd.myId;
   }
 
   public void stop(int id) {
-    executorService.submit(() -> {
+    soundHandler.post(() -> {
       for (Sound sound : sounds) {
         if (sound.myId == id) {
           sound.fading = true;
+          break;
         }
       }
     });
   }
-
-  private class SystemVolumeTask extends TimerTask {
-
-    @Override
-    public void run() {
-      if (Globals.IS_ARC) startVolume = .33333333333333333333f;
-      else {
-        if (Build.VERSION.SDK_INT >= 5 && !audioMan.isWiredHeadsetOn()) {
-          startVolume = Math.min(Globals.TWO_THIRDS, // Louder for internal speaker
-            (float) audioMan.getStreamVolume(STREAM_MUSIC) / maxSystemVolume);
-        } else {
-          startVolume = 1;
-        }
-      }
-    }
-
-  }
-
-  private class StopTask extends TimerTask {
-
-    @Override
-    public void run() {
-
-      for (Sound sound : sounds) {
-        if (sound.fading) {
-          sound.currVolume -= sound.volumeStep;
-          float vol = Math.max(0, sound.currVolume);
-          if (vol == 0) {
-            soundPool.stop(sound.osId);
-            sounds.remove(sound);
-            sound.free = true;
-          } else {
-            soundPool.setVolume(sound.osId, vol, vol);
-          }
-        }
-      }
-
-    }
-
-  }
-
 
 }
